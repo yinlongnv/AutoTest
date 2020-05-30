12476fd1a57c29e537bb6ae20fe2177aa4add6ad
yinlongnv <780894447@qq.com>
2020-05-30 16:43:41 +0800

    update

--- src/main/java/com/dadalong/autotest/bean/v1/wrapper/UserWrapper.java
+++ src/main/java/com/dadalong/autotest/bean/v1/wrapper/UserWrapper.java
@@ -27,10 +27,13 @@ public class UserWrapper extends QueryWrapper<User> {
      */
     public UserWrapper ofListWithSearch(SearchRequest request){
         Map<String,Object> map = request.getSearch();
+        // 筛选登录起止时间段
         Object startTime = map.get("startTime");
         Object endTime = map.get("endTime");
         this.ofTime(startTime, endTime);
+        // 筛选用户角色
         this.ofRole(map.get("role"));
+        // 筛选用户信息：用户名/用户编号
         Object search = map.get("userInfo");
         if (search != null && StringUtils.isNotBlank(search.toString())) {
             this.like("username", search.toString()).or().like("user_number", search.toString());
@@ -51,7 +54,7 @@ public class UserWrapper extends QueryWrapper<User> {
     }
 
     /**
-     * 筛选最后登录时间段
+     * 筛选最后登录起止时间段
      * @param startTime
      * @param endTime
      * @return
